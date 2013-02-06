8da6e0b83f7ac87d0ebf9a7e2dc44b2572673300
Barney Couch <barneycouch@gmail.com>
2013-02-06 01:39:56 +0000

    call_function  now included

---
+++ call_funcs.py
@@ -0,0 +1,3 @@
+from food_search import pretty_food_report
+
+print pretty_food_report('Salad')
--- food_search.py
+++ food_search.py
@@ -20,15 +20,25 @@ def food_report(food):
 	return occurrences		
 
 
-try:
-	food_report = food_report(str(sys.argv[1]))
-	if not len(food_report) == 0:
-		for k in food_report:
-			print "%s is served on:" % k
-			for i in food_report[k]:
+def pretty_food_report(food):
+	occurrences = food_report(food)
+	if not len(occurrences) == 0:
+		for k in occurrences:
+			print "%s is being served on:" % k
+			for i in occurrences[k]:
 				print i
 	else:
-		print "%s isn't being served in the near future!" % str(sys.argv[1])
-except:
-	print("Please provide the food you don't like:" "\n" """e.g. python food_search.py 'Cous Cous'""")
-	quit()
+		print "%s isn't being served in the near future!" % str(food)
+
+# try:
+# 	food_report = food_report(str(sys.argv[1]))
+# 	if not len(food_report) == 0:
+# 		for k in food_report:
+# 			print "%s is served on:" % k
+# 			for i in food_report[k]:
+# 				print i
+# 	else:
+# 		print "%s isn't being served in the near future!" % str(sys.argv[1])
+# except:
+# 	print("Please provide the food you don't like:" "\n" """e.g. python food_search.py 'Cous Cous'""")
+# 	quit()
